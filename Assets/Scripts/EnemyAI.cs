99ece5f4c1ebd758de83e3ad097abcb07dc3058d
lidiyaaaaa <lidochkarassokhina@yandex.ru>
2024-02-25 23:38:56 +0300

    Enemy

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform targetPoint;
+
+    private NavMeshAgent _navMeshAgent;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _navMeshAgent.destination = targetPoint.position;
+    }
+}
