b3c7798bc21146843d912b9e3094def6e8ad67e8
lidiyaaaaa <lidochkarassokhina@yandex.ru>
2024-03-04 12:38:45 +0300

    chase

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,20 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform targetPoint;
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
 
     private NavMeshAgent _navMeshAgent;
-    void Start()
+    private bool _isPlayerNoticed;
+
+    private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        PickNewPatrolPoint();
     }
 
-    void Update()
+    private void Update()
     {
-        _navMeshAgent.destination = targetPoint.position;
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        PatrolUpdate();
+    }
+
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+
+    private void PatrolUpdate()
+    {
+        if(!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
+        }       
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
     }
 }
